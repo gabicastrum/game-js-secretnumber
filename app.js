a55fb099cd50aa011168bf5d226dcae5cba35cdf
gabicastrum <g.laurindo@edu.pucrs.br>
2025-01-06 11:31:53 -0300

    inserindo tentativas

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5; // variável que guarda o número secreto
 console.log(numeroSecreto); // exibe o número secreto no console, apenas para testar
-let chute
-// colocar let antes do prompt para armazenar o valor digitado pelo usuário
+let chute;
+let tentativas = 1;
 
 // enquanto = while
 //enquanto o chute for diferente do número secreto, o jogo continua
@@ -11,12 +11,14 @@ while (chute != numeroSecreto) {
     chute = prompt('Escolha um número entre 1 e 30:');
     // condição = if
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto, que é ${numeroSecreto}`);
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
     } else {
         if (chute > numeroSecreto) {
             alert('Errou! O número secreto é menor que ' + chute);
         } else {
             alert('Errou! O número secreto é maior que ' + chute);
         }
+        //tentativas = tentativas + 1;, mas existe uma forma mais simples:
+        tentativas++;
     }
 }
