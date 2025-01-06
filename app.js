1c2c8f5d760bbea88aebf5c7b376e47a3c0a6c78
gabicastrum <g.laurindo@edu.pucrs.br>
2025-01-06 11:20:18 -0300

    novas condições

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 4; // variável que guarda o número secreto
+let numeroSecreto = 5; // variável que guarda o número secreto
 console.log(numeroSecreto); // exibe o número secreto no console'
 let chute = prompt('Escolha um número entre 1 e 30:'); // pede ao usuário para escolher um número
 // colocar let antes do prompt para armazenar o valor digitado pelo usuário
@@ -8,7 +8,9 @@ let chute = prompt('Escolha um número entre 1 e 30:'); // pede ao usuário para
 if (chute == numeroSecreto) {
     alert(`Isso ai! Você descobriu o número secreto, que é ${numeroSecreto}`);
 } else {
-    alert(`Que pena :( O número secreto não é ${chute}`);
+    if (chute > numeroSecreto) {
+        alert('Errou! O número secreto é menor que ' + chute);
+    } else {
+        alert('Errou! O número secreto é maior que ' + chute);
+    }
 }
-
-//Template string
