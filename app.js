b94616f251bd8d871b8dee1b84e0c0a2a8581edd
gabicastrum <g.laurindo@edu.pucrs.br>
2025-01-06 10:55:32 -0300

    concatenação - template string

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 5; // variável que guarda o número secreto
+let numeroSecreto = 4; // variável que guarda o número secreto
 console.log(numeroSecreto); // exibe o número secreto no console'
 let chute = prompt('Escolha um número entre 1 e 30:'); // pede ao usuário para escolher um número
 // colocar let antes do prompt para armazenar o valor digitado pelo usuário
 
 // condição = if
 if (chute == numeroSecreto) {
-    alert('Isso ai! Você descobriu o número secreto (5)');
+    alert(`Isso ai! Você descobriu o número secreto, que é ${numeroSecreto}`);
 } else {
-    alert('Que pena :( O número secreto não é ' + chute);
+    alert(`Que pena :( O número secreto não é ${chute}`);
 }
+
+//Template string
