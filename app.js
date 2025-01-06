a5320d16be881f5d96c44e5f6ccaead1e0a19715
gabicastrum <g.laurindo@edu.pucrs.br>
2025-01-06 11:26:22 -0300

    while/enquanto

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5; // variável que guarda o número secreto
-console.log(numeroSecreto); // exibe o número secreto no console'
-let chute = prompt('Escolha um número entre 1 e 30:'); // pede ao usuário para escolher um número
+console.log(numeroSecreto); // exibe o número secreto no console, apenas para testar
+let chute
 // colocar let antes do prompt para armazenar o valor digitado pelo usuário
 
-// condição = if
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto, que é ${numeroSecreto}`);
-} else {
-    if (chute > numeroSecreto) {
-        alert('Errou! O número secreto é menor que ' + chute);
+// enquanto = while
+//enquanto o chute for diferente do número secreto, o jogo continua
+// != não é igual
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 30:');
+    // condição = if
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto, que é ${numeroSecreto}`);
     } else {
-        alert('Errou! O número secreto é maior que ' + chute);
+        if (chute > numeroSecreto) {
+            alert('Errou! O número secreto é menor que ' + chute);
+        } else {
+            alert('Errou! O número secreto é maior que ' + chute);
+        }
     }
 }
