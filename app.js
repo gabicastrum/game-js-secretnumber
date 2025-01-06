f3a401e2f4cf5eb8ec8fcfe62da0a7c27740412f
gabicastrum <g.laurindo@edu.pucrs.br>
2025-01-06 11:46:53 -0300

    operador ternário

--- app.js
+++ app.js
@@ -23,8 +23,12 @@ while (chute != numeroSecreto) {
     }
 }
 
-if (tentativas == 1) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
-} else {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
-}
+//Operador ternário
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+//if (tentativas == 1) {
+   // alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
+//} else {
+    //alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+//}
