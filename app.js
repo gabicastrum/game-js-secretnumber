9af2e3b65a79744def9f79a03c197d6ad5c58593
gabicastrum <g.laurindo@edu.pucrs.br>
2025-01-06 10:31:41 -0300

    exibindo mensagens na tela

---
+++ app.js
@@ -0,0 +1,3 @@
+alert('Boas vindas ao jogo do número secreto!'); 
+let numeroSecreto = 29; // variável que guarda o número secreto
+prompt('Escolha um número entre 1 e 30:'); // pede ao usuário para escolher um número
