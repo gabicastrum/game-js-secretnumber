7ccffc94aa548483f54a2e6909be3d4c67e2f2a5
gabicastrum <g.laurindo@edu.pucrs.br>
2025-01-06 10:43:32 -0300

    comparando variaveis

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 alert('Boas vindas ao jogo do número secreto!'); 
 let numeroSecreto = 29; // variável que guarda o número secreto
-prompt('Escolha um número entre 1 e 30:'); // pede ao usuário para escolher um número
+let chute = prompt('Escolha um número entre 1 e 30:'); // pede ao usuário para escolher um número
+// colocar let antes do prompt para armazenar o valor digitado pelo usuário
+
+// condição = if
+if (numeroSecreto === 29) {
+    console.log ('Isso ai! Você descobriu o número secreto (29)');
+}
